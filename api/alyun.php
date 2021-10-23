c69b6806afc84ca85da610ca3dc0ce6f1890bba3
SkiMinoAPI <93034430+XMSMApi@users.noreply.github.com>
2021-10-23 23:10:39 +0800

    Add files via upload

--- api/alyun.php
+++ api/alyun.php
@@ -1,10 +1,4 @@
 <?php
-header("Connection: keep-alive");
-header("Cache-Control: max-age=0");
-header("Upgrade-Insecure-Requests: 1");
-header("User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64");
-header("Accept-Language: zh-CN,zh;q=0.9");
-
-header("Content-Type: Image/JPEG;Charset=UTF-8");
-imagejpeg(imagecreatefromjpeg('./logo.jpg'));
+header("Content-Type: Image/JPEG;Charset=UTF-8;referrer-policy: same-origin");
+imagejpeg(imagecreatefromjpeg('https://bj29.cn-beijing.data.alicloudccp.com/5fd9bc3c1e41ea78194849389d10c2d0e6917a57%2F5fd9bc3cd75846d85b4b41229a5c5ca993d0278e?x-oss-access-key-id=LTAIsE5mAn2F493Q&x-oss-additional-headers=referer&x-oss-expires=1635000966&x-oss-process=image%2Fresize%2Cw_1920%2Fformat%2Cjpeg&x-oss-signature=8uxlwoEsWikX72FM7RqB%2BWC2pXw4F4Hkjkjz%2F9xKLj8%3D&x-oss-signature-version=OSS2'));
 ?>
